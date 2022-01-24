3959ce9e26d778b68f0fcaffc94f86be79e67906
ck46 <prof.chakas@gmail.com>
2022-01-24 11:12:42 +0200

    Filter the List View

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,37 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Chansa Kabwe on 21/01/2022.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @State private var showFavoritesOnly = true
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
+    var body: some View {
+        NavigationView {
+            List(filteredLandmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
